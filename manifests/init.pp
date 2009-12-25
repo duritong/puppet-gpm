3d1bb01b17ff0fdddd8d57d516545eb970cd62f7
mh <mh@immerda.ch>
2009-12-25 21:00:39 +0100

    gpm as well on xen0 hosts

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,17 +2,15 @@
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "gpm": }
-
 class gpm {
-    case $virtual {
-        'physical': {    
-            case $operatingsystem {
-                gentoo: { include gpm::gentoo }
-                default: { include gpm::base }
-            }
-        }
-        # gpm makes only sense on physical hosts
-        default: { include gpm::disable }
+  case $virtual {
+    'physical','xen0': {    
+      case $operatingsystem {
+        gentoo: { include gpm::gentoo }
+        default: { include gpm::base }
+      }
     }
+    # gpm makes only sense on physical or xen0 hosts
+    default: { include gpm::disable }
+  }
 }
